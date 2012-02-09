f9170e6385128b4811ca926faa3e978645b28fa1
Kapil Easwar <groupflow.dev@gmail.com>
2012-02-09 13:17:54 -0500

    removed shared database

--- web.js
+++ web.js
@@ -1,43 +1,4 @@
 var express = require('express');
-var pg = require('pg');
-
-/* BEGIN EXAMPLE */
-var client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
-
-//queries are queued and executed one after another once the connection becomes available
-client.query("CREATE TEMP TABLE beatles(name varchar(10), height integer, birthday timestamptz)");
-client.query("INSERT INTO beatles(name, height, birthday) values($1, $2, $3)", ['Ringo', 67, new Date(1945, 11, 2)]);
-client.query("INSERT INTO beatles(name, height, birthday) values($1, $2, $3)", ['John', 68, new Date(1944, 10, 13)]);
-
-//queries can be executed either via text/parameter values passed as individual arguments
-//or by passing an options object containing text, (optional) parameter values, and (optional) query name
-client.query({
-  name: 'insert beatle',
-  text: "INSERT INTO beatles(name, height, birthday) values($1, $2, $3)",
-  values: ['George', 70, new Date(1946, 02, 14)]
-});
-
-//subsequent queries with the same name will be executed without re-parsing the query plan by postgres
-client.query({
-  name: 'insert beatle',
-  values: ['Paul', 63, new Date(1945, 04, 03)]
-});
-var query = client.query("SELECT * FROM beatles WHERE name = $1", ['John']);
-
-//can stream row results back 1 at a time
-query.on('row', function(row) {
-  console.log(row);
-  console.log("Beatle name: %s", row.name); //Beatle name: John
-  console.log("Beatle birth year: %d", row.birthday.getYear()); //dates are returned as javascript dates
-  console.log("Beatle height: %d' %d\"", Math.floor(row.height/12), row.height%12); //integers are returned as javascript ints
-});
-
-//fired after last row is emitted
-query.on('end', function() { 
-  client.end();
-});
-/* END EXAMPLE */
 
 var app = express.createServer(express.logger());
 
