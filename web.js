660a75d696647d8287e7d8fd17158c740382997b
Kapil Easwar <groupflow.dev@gmail.com>
2012-02-10 11:22:01 -0500

    trying mysql module

--- web.js
+++ web.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var mysql = require('mysql');
+var db = mysql.createClient({
+	host: process.env.RDS_ENDPOINT,
+	port: process.env.RDS_PORT,
+	user: process.env.RDS_USERNAME,
+	password: process.env.RDS_PASSWORD,
+	database: process.env.RDS_DATABASE
+});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(req, res) {
-	res.send('Hello world! - from ' + process.env.APP_NAME);
+	console.log(process.env.RDS_ENDPOINT);
+	console.log(process.env.RDS_PORT);
+	console.log(process.env.RDS_USERNAME);
+	console.log(process.env.RDS_PASSWORD);
+	console.log(process.env.RDS_DATABASE);
+	
+	db.query('select * from UsersMeta', function(err, results, fields) {
+		console.log(err);
+		console.log(results);
+		console.log(fields);
+		res.send('Hello world! - from ' + process.env.APP_NAME);
+	});
 });
 
 var port = process.env.PORT || 8080;
