c2a0d4574ff968c2aa7c2a791a4cb18f29b86dee
Kapil Easwar <groupflow.dev@gmail.com>
2012-02-11 17:21:58 -0500

    added landing page

--- modules/gen-utils.js
+++ modules/gen-utils.js
@@ -1,10 +1,45 @@
 /* gets the params from the request */
 exports.getParams = function(req) {
-	if (!req || !req.method) return {};
+	if (!req) {
+		console.log('getParams() called on undefined request');
+		return {};
+	}
+	else if (!req.method) {
+		console.log('getParams() called on request with no method: ');
+		console.log(req);
+	}
 	else if (req.method == 'GET') return req.query;
 	else if (req.method == 'POST') return req.body;
 	else {
 		console.log('Handling request that is neither GET nor POST: need to update getParams()')
 		return {};
+	}
+}
+
+/* renders a page with some default view variables */
+exports.render = function(req, res, path, vars) {
+	req = req || {};
+	vars = vars || {};
+	
+	//use default render vars unless otherwise specified
+	var dflt_vars = this.dft_render_vars(req);
+	for (var i in dflt_vars) {
+		if (typeof vars[i] == 'undefined')
+			vars[i] = dflt_vars[i];
+	}
+
+	res.render(path, vars);
+}
+
+/* default render vars */
+exports.dft_render_vars = function(req) {
+	req = req || {};
+	return {
+		layout: false,
+		resources: {},
+		user: req.user,
+		request: {
+			params: this.getParams(req)
+		}
 	}
 }
--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var gen_utils = require('./modules/gen-utils.js');
 
 //configure authentication
 //TODO
@@ -10,13 +11,16 @@ var app = express.createServer(
 	express.session({ secret: process.env.SESSION_SECRET })
 	//TODO add everyauth middleware
 );
+app.use('/public', express.static(__dirname + '/public'));
+//TODO everyauth.helpExpress(app);
 
 //configure the api
 require('./modules/api.js').configure(app);
 
 /* BEGIN PAGE ROUTING *******************************************/
 app.get('/', function(req, res) {
-	res.send('Hello world! - from ' + process.env.APP_NAME);
+	if (req.user) /* TODO go to a different page */ gen_utils.render(req, res, 'landing.ejs');
+	else gen_utils.render(req, res, 'landing.ejs');
 });
 /* END PAGE ROUTING *********************************************/
 
