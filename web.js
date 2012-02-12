404c6cd1f50d32987b8f859f1219d059411d5ec0
Kapil Easwar <groupflow.dev@gmail.com>
2012-02-12 15:20:52 -0500

    updated notes

--- web.js
+++ web.js
@@ -1,20 +1,15 @@
 var express = require('express');
 var gen_utils = require('./modules/gen-utils.js');
 
-//configure authentication
-//TODO
-
 var app = express.createServer(
 	express.logger(),
 	express.bodyParser(),
 	express.cookieParser(),
 	express.session({ secret: process.env.SESSION_SECRET })
-	//TODO add everyauth middleware
 );
 app.use('/public', express.static(__dirname + '/public'));
-//TODO everyauth.helpExpress(app);
 
-//configure the api
+//configure the api (which includes auth)
 require('./modules/api.js').configure(app);
 
 /* BEGIN PAGE ROUTING *******************************************/
