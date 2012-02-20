39112f6e9881c46c82e86fc8710c9d914fc365fe
Kapil Easwar <groupflow.dev@gmail.com>
2012-02-19 19:00:04 -0500

    started framework of friends api, and updated todo

--- modules/api-v0.0.0/api/friends.js
+++ modules/api-v0.0.0/api/friends.js
@@ -1,7 +1,58 @@
+/*
+	This API domain:
+		/api/friends, api.friends
+
+	REST functions:
+		list - lists the friends of the auth'd user
+	
+	Internal-only functions:
+		(none)
+
+	Directly touches database tables:
+		(none) TODO
+
+	Directly touches session variables:
+		(none) TODO
+*/
+var api_utils = require('./util/api-utils.js');
+var api_errors = require('./util/api-errors.js');
+var api_validate = require('./util/api-validate.js');
+
 function configure(app, url_prefix) {
 	url_prefix += '/friends';
 
 	//TODO
 }
 exports.configure = configure;
+
+/*
+	Inputs:
+		offset (optional, default 0) - the offset of the list of friends
+		maxcount (optional, default to the max of 50) - the max number of friends to return
+		sort (optional, defaults to 'username') - how the results should be sorted
+			'username', 'name', 'length'
+
+	Cases:
+		error: no auth'd user
+		success: the list of (TODO: id's or user objects?)
+		TODO
+*/
+function list(req, params, callback) {
+	//TODO
+}
+exports.list = list;
+
+/*
+	Checks whether the auth'd user is friends with another user
+
+	Inputs:
+		username (required) - the username of a user
+
+	Cases
+		TODO
+*/
+function is(req, params, callback) {
+	//TODO
+}
+exports.is = is;
 
