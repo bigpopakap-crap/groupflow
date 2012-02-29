20d5e2a17fcc62d7f108d3761cd21b553fd2a615
Kapil Easwar <groupflow.dev@gmail.com>
2012-02-29 16:55:40 -0500

    added the /friends/is function

--- modules/api-v0.0.0/api/friends.js
+++ modules/api-v0.0.0/api/friends.js
@@ -9,7 +9,7 @@
 		(none)
 
 	Directly touches database tables:
-		(none) TODO
+		Friendships (read/write)
 
 	Directly touches session variables:
 		(none) TODO
@@ -17,11 +17,14 @@
 var api_utils = require('./util/api-utils.js');
 var api_errors = require('./util/api-errors.js');
 var api_validate = require('./util/api-validate.js');
+var db = require('../db.js');
 
 function configure(app, url_prefix) {
 	url_prefix += '/friends';
 
-	//TODO
+	//actions in this api domain
+	app.get(url_prefix + '/list', api_utils.restHandler(list));
+	app.get(url_prefix + '/is', api_utils.restHandler(is));
 }
 exports.configure = configure;
 
@@ -49,10 +52,46 @@ exports.list = list;
 		username (required) - the username of a user
 
 	Cases
-		TODO
+		Error: missing params, database error
+		Success: bool
+			true if the auth'd user is friends with the given username
+			false otherwise (not friends, or username doesn't exist)
 */
 function is(req, params, callback) {
-	//TODO
+	//make sure the username is there
+	var paramErrors = api_validate.validate(params, {
+		username: { required: true }
+	});
+
+	//check for auth'd user and errors on the input
+	if (!req.session.user) {
+		//no auth'd user
+		return callback(api_errors.noAuth(req.session.user, params));
+	}
+	else if (paramErrors) {
+		return callback(api_errors.badFormParams(req.session.user, params, paramErrors));
+	}
+	else {
+		//order the usernames
+		var usernames = [ req.session.user.username, params.username ].sort();
+		db.query(
+			'select * from Friendships where lesser=? and greater=? limit 1',
+			usernames,
+			function (err, results) {
+				if (err) {
+					//return a database error
+					return callback(api_errors.database(req.session.user, params, err));
+				}
+				else {
+					//return true iff the results array is not empty
+					return callback(api_utils.wrapResponse({
+						params: params,
+						success: (results.length > 0 ? true : false)
+					}));
+				}
+			}
+		);
+	}
 }
 exports.is = is;
 
